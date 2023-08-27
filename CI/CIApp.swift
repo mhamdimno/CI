f65f2279835181cd139de0dee1c5e1751a4dfbd1
bob <bob@>
2023-08-27 07:26:35 +0300

    first commit

---
+++ CI/CIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CIApp.swift
+//  CI
+//
+//  Created by A One Way To Allah on 27/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct CIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
